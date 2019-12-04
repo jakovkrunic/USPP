39e003a5546fdedfcf1f82f0cd8415a8bbdfa067
grdavor <grdavor@student.math.hr>
2019-12-04 11:11:49 +0100

    Added All_renumberings function

--- CE.py
+++ CE.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from numpy import linalg as la
 from scipy.linalg import fractional_matrix_power
 from sklearn.cluster import KMeans
+from itertools import *
 import math
 
 
@@ -18,9 +19,27 @@ def CE(C1,C2):
                     if(C1[l] == i and C2[l] == j):
                         nij+=1
                 suma+=nij
-                
-            
+
+
     return suma/n
-        
+
+def All_renumberings(C1,C2):
+    k = max(C1)+1   #broj klastera oba klasteriranja mora biti isti
+    min=CE(C1,C2)
+    for permutation in list(permutations(range(k))):
+        new_renumber =[]
+        dictionary=dict(zip(range(k), permutation))
+        for cluster_number in C1:
+            new_renumber.append(dictionary[cluster_number])
+        candidate=CE(new_renumber,C2)
+        if(min>candidate):
+            min=candidate
+    return min
+
 print(CE([1,1,0,2,2],[2,0,0,1,1]))
+print(CE([1,1,0,2,2],[2,2,1,0,0]))
 print(CE([0,1,0],[0,1,0]))
+
+print (All_renumberings([1,1,0,2,2],[2,0,0,1,1]))
+print (All_renumberings([1,1,0,2,2],[2,2,1,0,0]))
+print (All_renumberings([0,1,0],[0,1,0]))
